39555ea6ad22c4ef8c2d27b4dcf4c04632d96c15
NoChance <sergio.ds20@gmail.com>
2013-08-22 22:12:27 +0100

    Stop Commands for In Combat

--- src/main/java/me/NoChance/PvPManager/CommandListener.java
+++ src/main/java/me/NoChance/PvPManager/CommandListener.java
@@ -1,5 +1,6 @@
 package me.NoChance.PvPManager;
 
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
@@ -15,8 +16,10 @@ public class CommandListener implements Listener{
 	
 	@EventHandler
 	public void onCommand(PlayerCommandPreprocessEvent event){
-		if(plugin.inCombat.contains(event.getPlayer().getName()))
-			event.setCancelled(true);	
+		if(plugin.inCombat.contains(event.getPlayer().getName())){
+			event.setCancelled(true);
+			event.getPlayer().sendMessage(ChatColor.RED + "Command denied! You are still in combat!");
+		}
 	}
 
 }
