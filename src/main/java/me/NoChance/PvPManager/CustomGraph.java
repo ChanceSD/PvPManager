9fc50930350835c97a649163d4e295c7479bf820
NoChance <sergio.ds20@gmail.com>
2013-09-03 00:25:47 +0100

    Changed somethings in Metrics

--- src/main/java/me/NoChance/PvPManager/CustomGraph.java
+++ src/main/java/me/NoChance/PvPManager/CustomGraph.java
@@ -2,6 +2,8 @@ package me.NoChance.PvPManager;
 
 import java.io.IOException;
 
+import me.NoChance.PvPManager.Config.Variables;
+
 import org.mcstats.Metrics;
 import org.mcstats.Metrics.Graph;
 
@@ -18,6 +20,131 @@ public class CustomGraph {
 	try {
 	Metrics metrics = new Metrics(plugin);
 	Graph keepItemsExp = metrics.createGraph("Percentage of Keep and Drop");
+	Graph pvpTimerUsage = metrics.createGraph("PvPTimer Usage");
+	Graph disableFly = metrics.createGraph("Disable Fly feature");
+	Graph inCombatTime = metrics.createGraph("Time in Combat");
+	
+	inCombatTime.addPlotter(new Metrics.Plotter("15 seconds") {
+		@Override
+		public int getValue() {
+			int i = 0;
+			if (Variables.timeInCombat == 15)
+				i++;
+			
+			return i;
+		}
+	});
+	
+	inCombatTime.addPlotter(new Metrics.Plotter("Between 10 and 15 seconds") {
+		@Override
+		public int getValue() {
+			int i = 0;
+			if (Variables.timeInCombat < 15 && Variables.timeInCombat >= 10)
+				i++;
+			
+			return i;
+		}
+	});
+	
+	inCombatTime.addPlotter(new Metrics.Plotter("Less than 10 seconds") {
+		@Override
+		public int getValue() {
+			int i = 0;
+			if (Variables.timeInCombat < 10)
+				i++;
+			
+			return i;
+		}
+	});
+	
+	inCombatTime.addPlotter(new Metrics.Plotter("Between 15 and 30 seconds") {
+		@Override
+		public int getValue() {
+			int i = 0;
+			if (Variables.timeInCombat > 15 && Variables.timeInCombat <= 30)
+				i++;
+			
+			return i;
+		}
+	});
+	
+	inCombatTime.addPlotter(new Metrics.Plotter("Between 31 and 45 seconds") {
+		@Override
+		public int getValue() {
+			int i = 0;
+			if (Variables.timeInCombat > 30 && Variables.timeInCombat <= 45)
+				i++;
+			
+			return i;
+		}
+	});
+	
+	inCombatTime.addPlotter(new Metrics.Plotter("Between 46 and 60 seconds") {
+		@Override
+		public int getValue() {
+			int i = 0;
+			if (Variables.timeInCombat > 45 && Variables.timeInCombat <= 60)
+				i++;
+			
+			return i;
+		}
+	});
+	
+	inCombatTime.addPlotter(new Metrics.Plotter("More than 60 seconds") {
+		@Override
+		public int getValue() {
+			int i = 0;
+			if (Variables.timeInCombat > 60)
+				i++;
+			
+			return i;
+		}
+	});
+	
+	pvpTimerUsage.addPlotter(new Metrics.Plotter("Enabled") {
+		@Override
+		public int getValue() {
+			int i = 0;
+			if (Variables.pvpTimerEnabled)
+				i++;
+			
+			return i;
+		}
+	});
+	
+	pvpTimerUsage.addPlotter(new Metrics.Plotter("Disabled") {
+		@Override
+		public int getValue() {
+			int i = 0;
+			if (!Variables.pvpTimerEnabled)
+				i++;
+			
+			return i;
+		}
+	});
+	
+	disableFly.addPlotter(new Metrics.Plotter("Enabled") {
+		@Override
+		public int getValue() {
+			int i = 0;
+			if (Variables.disableFly)
+				i++;
+			
+			return i;
+		}
+	});
+	
+	disableFly.addPlotter(new Metrics.Plotter("Disabled") {
+		@Override
+		public int getValue() {
+			int i = 0;
+			if (!Variables.disableFly)
+				i++;
+			
+			return i;
+		}
+	});
+	
 	keepItemsExp.addPlotter(new Metrics.Plotter("Keep Everything") {
 		@Override
 		public int getValue() {
