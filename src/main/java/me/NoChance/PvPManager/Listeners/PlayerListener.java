938dce3fb34c90651743aa7534cc5de9edba20cb
NoChanceSD <sergio.ds20@gmail.com>
2014-03-02 01:31:01 +0000

    New Feature - Auto Soup, Soup instantly regenerates health

--- src/main/java/me/NoChance/PvPManager/Listeners/PlayerListener.java
+++ src/main/java/me/NoChance/PvPManager/Listeners/PlayerListener.java
@@ -5,12 +5,18 @@ import me.NoChance.PvPManager.PvPlayer;
 import me.NoChance.PvPManager.Config.Messages;
 import me.NoChance.PvPManager.Config.Variables;
 import me.NoChance.PvPManager.Managers.PlayerHandler;
+import me.NoChance.PvPManager.Utils.Utils;
+
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -90,6 +96,23 @@ public class PlayerListener implements Listener {
 	}
 
 	@EventHandler
+	public void onPlayerInteract(PlayerInteractEvent e) {
+		Player player = e.getPlayer();
+		if (Utils.PMAllowed(player.getWorld().getName()) && Variables.autoSoupEnabled) {
+			if (player.getHealth() == player.getMaxHealth())
+				return;
+			if (player.getItemInHand().getType() == Material.MUSHROOM_SOUP) {
+				if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+					player.setHealth(player.getHealth() + Variables.soupHealth > player.getMaxHealth() ? player.getMaxHealth() : player
+							.getHealth() + Variables.soupHealth);
+					player.getItemInHand().setType(Material.BOWL);
+					return;
+				}
+			}
+		}
+	}
+
+	@EventHandler
 	public void onPlayerLogin(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
 		ph.get(player);
@@ -127,4 +150,23 @@ public class PlayerListener implements Listener {
 		}
 	}
 
+	@EventHandler(ignoreCancelled = true)
+	public void onCommand(PlayerCommandPreprocessEvent event) {
+		if (Variables.stopCommands && Variables.inCombatEnabled) {
+			if (plugin.getPlayerHandler().get(event.getPlayer()).isInCombat()) {
+				if (!Variables.commandsAllowed.contains(event.getMessage().substring(1).split(" ")[0])) {
+					event.setCancelled(true);
+					event.getPlayer().sendMessage(Messages.Command_Denied_InCombat);
+				}
+			}
+		}
+		// Checking if PvPTimer is right every time a command is executed
+		// Using this instead of a repeating task, seems less resource intensive
+		if (Variables.pvpTimerEnabled) {
+			if (plugin.getWtm().isPvpTimerWorld(event.getPlayer().getWorld())) {
+				plugin.getWtm().getPvpTimer(event.getPlayer().getWorld()).checkWorldPvP();
+			}
+		}
+
+	}
 }
