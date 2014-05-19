a868c93f50b812af474c8dc2623608843f14c0be
NoChanceSD <nochance20@gmail.com>
2014-05-19 19:39:22 +0100

    Don't check for WorldGuard flags anymore

---
+++ src/main/java/me/NoChance/PvPManager/Listeners/WGListener.java
@@ -0,0 +1,17 @@
+package me.NoChance.PvPManager.Listeners;
+
+import me.NoChance.PvPManager.Utils.CombatUtils;
+import me.NoChance.PvPManager.Utils.CombatUtils.CancelResult;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import com.sk89q.worldguard.protection.events.DisallowedPVPEvent;
+
+public class WGListener implements Listener {
+
+	@EventHandler
+	public void onWGPvPCancel(DisallowedPVPEvent event) {
+		if (CombatUtils.tryCancel(event.getAttacker(), event.getDefender()) == CancelResult.FAIL_OVERRIDE)
+			event.setCancelled(true);
+	}
+
+}
