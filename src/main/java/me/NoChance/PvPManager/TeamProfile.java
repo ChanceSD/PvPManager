cd8efd020af3e3837ff83b665a61939fbf458685
NoChanceSD <nochance20@gmail.com>
2014-11-18 21:48:57 +0000

    Add possibility to disable one nametag for PvP on/off

--- src/main/java/me/NoChance/PvPManager/TeamProfile.java
+++ src/main/java/me/NoChance/PvPManager/TeamProfile.java
@@ -1,6 +1,7 @@
 package me.NoChance.PvPManager;
 
 import me.NoChance.PvPManager.Config.Variables;
+
 import org.bukkit.ChatColor;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
@@ -25,14 +26,14 @@ public class TeamProfile {
 				inCombat = scoreboard.registerNewTeam("InCombat");
 			inCombat.setPrefix(ChatColor.translateAlternateColorCodes('&', Variables.nameTagColor));
 		}
-		if (pvpOn == null) {
+		if (pvpOn == null && !Variables.toggleColorOn.equalsIgnoreCase("none")) {
 			if (scoreboard.getTeam("PvPOn") != null)
 				pvpOn = scoreboard.getTeam("PvPOn");
 			else
 				pvpOn = scoreboard.registerNewTeam("PvPOn");
 			pvpOn.setPrefix(ChatColor.translateAlternateColorCodes('&', Variables.toggleColorOn));
 		}
-		if (pvpOff == null) {
+		if (pvpOff == null && !Variables.toggleColorOff.equalsIgnoreCase("none")) {
 			if (scoreboard.getTeam("PvPOff") != null)
 				pvpOff = scoreboard.getTeam("PvPOff");
 			else
@@ -58,9 +59,16 @@ public class TeamProfile {
 	}
 
 	public void setPvP(boolean state) {
-		if (state)
-			pvpOn.addPlayer(pvPlayer.getPlayer());
-		else
-			pvpOff.addPlayer(pvPlayer.getPlayer());
+		if (state) {
+			if (pvpOn == null)
+				restoreTeam();
+			else
+				pvpOn.addPlayer(pvPlayer.getPlayer());
+		} else {
+			if (pvpOff == null)
+				restoreTeam();
+			else
+				pvpOff.addPlayer(pvPlayer.getPlayer());
+		}
 	}
 }
