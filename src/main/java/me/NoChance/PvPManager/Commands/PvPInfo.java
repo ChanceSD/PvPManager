56d6e51bca2718f3e73b15e1ae7d6b36eb0bb6d6
NoChanceSD <nochance20@gmail.com>
2015-05-17 19:15:07 +0100

    Added /pvpinfo [player] command

---
+++ src/main/java/me/NoChance/PvPManager/Commands/PvPInfo.java
@@ -0,0 +1,48 @@
+package me.NoChance.PvPManager.Commands;
+
+import me.NoChance.PvPManager.PvPlayer;
+import me.NoChance.PvPManager.Managers.PlayerHandler;
+import me.NoChance.PvPManager.Utils.CombatUtils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class PvPInfo implements CommandExecutor {
+
+	private PlayerHandler ph;
+
+	public PvPInfo(PlayerHandler ph) {
+		this.ph = ph;
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (args.length == 0 && sender instanceof Player) {
+			sendInfo(sender, ph.get((Player) sender));
+			return true;
+		} else if (args.length == 1 && sender.hasPermission("pvpmanager.info.others")) {
+			if (CombatUtils.isOnline(args[0])) {
+				sendInfo(sender, ph.get(Bukkit.getPlayer(args[0])));
+				return true;
+			} else {
+				sender.sendMessage(ChatColor.DARK_RED + args[0] + " Does Not Exist or is Offline");
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private void sendInfo(CommandSender sender, PvPlayer target) {
+		sender.sendMessage(ChatColor.YELLOW + "§lPvPManager Info");
+		sender.sendMessage(ChatColor.GREEN + "- Name: §f" + target.getName());
+		sender.sendMessage(ChatColor.GREEN + "- PvP Status: §f" + target.hasPvPEnabled());
+		sender.sendMessage(ChatColor.GREEN + "- Tagged: §f" + target.isInCombat());
+		sender.sendMessage(ChatColor.GREEN + "- Newbie: §f" + target.isNewbie());
+		sender.sendMessage(ChatColor.GREEN + "- World: §f" + target.getWorldName());
+		sender.sendMessage(ChatColor.GREEN + "- Override: §f" + target.hasOverride());
+	}
+
+}
