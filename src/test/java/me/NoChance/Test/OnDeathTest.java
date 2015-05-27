7818effb51677637243bbd7f3560bfba4d7cc270
NoChanceSD <nochance20@gmail.com>
2015-05-28 00:41:03 +0100

    Tests fixed

---
+++ src/test/java/me/NoChance/Test/OnDeathTest.java
@@ -0,0 +1,54 @@
+package me.NoChance.Test;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+import me.NoChance.PvPManager.PvPManager;
+import me.NoChance.PvPManager.Listeners.PlayerListener;
+import me.NoChance.PvPManager.Utils.CombatUtils;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Answers;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+
+public class OnDeathTest {
+
+	private static PvPManager plugin;
+	private static PlayerListener listener;
+	private PlayerDeathEvent event;
+	@Mock(answer = Answers.RETURNS_MOCKS)
+	private Player attacker;
+	@Mock(answer = Answers.RETURNS_MOCKS)
+	private Player defender;
+
+	@BeforeClass
+	public static void setupClass() {
+		PluginTest pt = AllTests.pt;
+		plugin = pt.getPlugin();
+		PowerMockito.mockStatic(CombatUtils.class);
+		when(CombatUtils.isWorldAllowed(anyString())).thenReturn(true);
+		when(CombatUtils.isPvP((EntityDamageByEntityEvent) Matchers.anyObject())).thenCallRealMethod();
+		listener = new PlayerListener(plugin);
+	}
+
+	@Before
+	public final void setup() {
+		MockitoAnnotations.initMocks(this);
+		when(attacker.hasMetadata(Matchers.anyString())).thenReturn(false);
+		when(defender.hasMetadata(Matchers.anyString())).thenReturn(false);
+		event = new PlayerDeathEvent(defender, null, 0, null);
+	}
+
+	@Test
+	public final void dropsEnabled() {
+		listener.onPlayerDeath(event);
+	}
+
+}
