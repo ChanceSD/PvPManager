fbb41c0a382f19b8449326fd9da5e73a25968163
NoChanceSD <nochance20@gmail.com>
2015-05-29 20:46:33 +0100

    Added option to manage player drops! The modes are: Drop only in PvP, Keep only in PvP, Transfer drops directly to killer and Always drop where PvPManager does nothing so items drop as usual.

---
+++ src/main/java/me/NoChance/PvPManager/Dependencies/WorldGuard.java
@@ -0,0 +1,25 @@
+package me.NoChance.PvPManager.Dependencies;
+
+import me.NoChance.PvPManager.PvPlugin;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import com.sk89q.worldguard.bukkit.RegionQuery;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.flags.DefaultFlag;
+
+public class WorldGuard implements PvPlugin {
+
+	private final RegionQuery rq;
+
+	public WorldGuard(final Plugin plugin) {
+		this.rq = ((WorldGuardPlugin) plugin).getRegionContainer().createQuery();
+	}
+
+	public final boolean canAttack(final Player attacker, final Player defender) {
+		boolean flag = rq.testState(defender.getLocation(), defender, DefaultFlag.PVP);
+		return flag;
+	}
+
+}
