e56032911db0690ed7df532357b5e42db592c933
NoChanceSD <nochance20@gmail.com>
2015-06-02 23:15:31 +0100

    Added German translation. Thanks MrMusicBoy12345

--- src/main/java/me/NoChance/PvPManager/Config/Locale.java
+++ src/main/java/me/NoChance/PvPManager/Config/Locale.java
@@ -1,7 +1,7 @@
 package me.NoChance.PvPManager.Config;
 
 public enum Locale {
-	EN("messages.properties"), RU("messages_ru.properties"), PT_BR("messages_ptBR.properties"), CH("messages_ch.properties"), ES("messages_es.properties");
+	EN("messages.properties"), RU("messages_ru.properties"), CH("messages_ch.properties"), ES("messages_es.properties"), DE("messages_de.properties");
 
 	private final String fileName;
 
