049b6f31c79edb9c9ad2654ddbeb9a36869f3edd
NoChanceSD <nochance20@gmail.com>
2015-06-11 22:41:47 +0100

    Fix players from same Faction attacking each other inside WG region with PvP Allow Also, some internal changes for better (maybe?)

--- src/main/java/me/NoChance/PvPManager/Utils/CancelResult.java
+++ src/main/java/me/NoChance/PvPManager/Utils/CancelResult.java
@@ -1,5 +1,17 @@
 package me.NoChance.PvPManager.Utils;
 
 public enum CancelResult {
-	NEWBIE, NEWBIE_OTHER, PVPDISABLED, PVPDISABLED_OTHER, RESPAWN_PROTECTION, FAIL, FAIL_OVERRIDE
+	NEWBIE, PVPDISABLED, RESPAWN_PROTECTION, FAIL, FAIL_OVERRIDE;
+
+	private boolean isAttacker;
+
+	// If the attack was cancelled because of the attacker status or not
+	public boolean attackerCaused() {
+		return isAttacker;
+	}
+
+	public CancelResult setAttackerCaused(boolean attacker) {
+		isAttacker = attacker;
+		return this;
+	}
 }
