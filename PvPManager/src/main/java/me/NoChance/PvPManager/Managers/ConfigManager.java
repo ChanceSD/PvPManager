ceb4a192039d54d35d811c75ca75c13910795d4f
NoChanceSD <nochance20@gmail.com>
2015-07-22 18:48:00 +0100

    Move project to sub directory

---
+++ PvPManager/src/main/java/me/NoChance/PvPManager/Managers/ConfigManager.java
@@ -0,0 +1,162 @@
+package me.NoChance.PvPManager.Managers;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+import me.NoChance.PvPManager.PvPManager;
+import me.NoChance.PvPManager.Version;
+import me.NoChance.PvPManager.Config.Config;
+import me.NoChance.PvPManager.Config.Variables;
+import me.NoChance.PvPManager.Utils.Log;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+public class ConfigManager {
+
+	private final PvPManager plugin;
+	private final File usersFile;
+	private final YamlConfiguration users;
+	private Config config;
+	private int configVersion;
+
+	public ConfigManager(final PvPManager plugin) {
+		this.plugin = plugin;
+		this.users = new YamlConfiguration();
+		this.usersFile = new File(plugin.getDataFolder(), "users.yml");
+		configVersion = plugin.getConfig().getInt("Config Version", 0);
+		loadConfig();
+		loadUsers();
+	}
+
+	private void updateDefaultConfig() {
+		this.config.set("Default PvP", Variables.isDefaultPvp());
+		this.config.set("PvP Blood", Variables.isPvpBlood());
+		this.config.set("Auto Soup.Enabled", Variables.isAutoSoupEnabled());
+		this.config.set("Auto Soup.Health Gain", Variables.getSoupHealth());
+		this.config.set("Disable Fly", Variables.isDisableFly());
+		this.config.set("Disable GameMode", Variables.isDisableGamemode());
+		this.config.set("Disable Disguise", Variables.isDisableDisguise());
+		this.config.set("Disable Invisibility", Variables.isDisableInvisibility());
+		this.config.set("Ignore Zones For Tagged", Variables.isStopBorderHopping());
+		this.config.set("Ignore No Damage Hits", Variables.isIgnoreNoDamageHits());
+
+		this.config.set("In Combat.Enabled", Variables.isInCombatEnabled());
+		this.config.set("In Combat.Silent", Variables.isInCombatSilent());
+		this.config.set("In Combat.Time(seconds)", Variables.getTimeInCombat());
+		this.config.set("In Combat.Name Tag Color", Variables.getNameTagColor());
+		this.config.set("In Combat.Only Tag Attacker", Variables.isOnlyTagAttacker());
+		this.config.set("In Combat.Block EnderPearl", Variables.isBlockEnderPearl());
+		this.config.set("In Combat.Block Place Blocks", Variables.isBlockPlaceBlocks());
+		this.config.set("In Combat.Stop Commands.Enabled", Variables.isStopCommands());
+		this.config.set("In Combat.Stop Commands.Whitelist", Variables.isCommandsWhitelist());
+		this.config.set("In Combat.Stop Commands.Commands", Variables.getCommandsAllowed());
+		this.config.set("In Combat.Punishments.Enabled", Variables.isPunishmentsEnabled());
+		this.config.set("In Combat.Punishments.Broadcast PvPLog", Variables.isBroadcastPvpLog());
+		this.config.set("In Combat.Punishments.Log To File", Variables.isLogToFile());
+		this.config.set("In Combat.Punishments.Kill on Logout.Enabled", Variables.isKillOnLogout());
+		this.config.set("In Combat.Punishments.Kill on Logout.Drops.Inventory", Variables.isDropInventory());
+		this.config.set("In Combat.Punishments.Kill on Logout.Drops.Experience", Variables.isDropExp());
+		this.config.set("In Combat.Punishments.Kill on Logout.Drops.Armor", Variables.isDropArmor());
+		this.config.set("In Combat.Punishments.Fine.Enabled", Variables.isFineEnabled());
+		this.config.set("In Combat.Punishments.Fine.Amount", Variables.getFineAmount());
+
+		this.config.set("Player Kills.Money Reward", Variables.getMoneyReward());
+		this.config.set("Player Kills.Money Penalty", Variables.getMoneyPenalty());
+		this.config.set("Player Kills.Commands On Kill.Enabled", Variables.isCommandsOnKillEnabled());
+		this.config.set("Player Kills.Commands On Kill.Commands", Variables.getCommandsOnKill());
+
+		this.config.set("PvP Toggle.Cooldown(seconds)", Variables.getToggleCooldown());
+		this.config.set("PvP Toggle.Broadcast", Variables.isToggleBroadcast());
+		this.config.set("PvP Toggle.NameTags.Enabled", Variables.isToggleNametagsEnabled());
+		this.config.set("PvP Toggle.NameTags.Color On", Variables.getToggleColorOn());
+		this.config.set("PvP Toggle.NameTags.Color Off", Variables.getToggleColorOff());
+
+		this.config.set("Kill Abuse.Enabled", Variables.isKillAbuseEnabled());
+		this.config.set("Kill Abuse.Max Kills", Variables.getKillAbuseMaxKills());
+		this.config.set("Kill Abuse.Time Limit", Variables.getKillAbuseTime());
+		this.config.set("Kill Abuse.Commands on Abuse", Variables.getKillAbuseCommands());
+		this.config.set("Kill Abuse.Respawn Protection", Variables.getRespawnProtection());
+
+		this.config.set("Newbie Protection.Enabled", Variables.isNewbieProtectionEnabled());
+		this.config.set("Newbie Protection.Time(minutes)", Variables.getNewbieProtectionTime());
+		this.config.set("Newbie Protection.Block Pick Items", Variables.isBlockPickNewbies());
+		this.config.set("Newbie Protection.Protect From Everything", Variables.isNewbieGodMode());
+
+		this.config.set("Update Check.Enabled", Variables.isUpdateCheck());
+		this.config.set("Update Check.Update Location", Variables.getUpdateLocation());
+		this.config.set("Update Check.Auto Update", Variables.isUpdate());
+
+		this.config.set("World Exclusions", Variables.getWorldsExcluded());
+		this.config.saveConfig();
+	}
+
+	private void loadConfig() {
+		if (getConfigVersion() < Integer.parseInt(Version.getConfigVersion())) {
+			final File configFile = new File(plugin.getDataFolder(), "config.yml");
+			if (configFile.exists()) {
+				config = new Config(plugin, "config.yml");
+				Variables.initizalizeVariables(this);
+				configFile.delete();
+				config = new Config(plugin, "config.yml");
+				updateDefaultConfig();
+				Variables.setConfigUpdated(true);
+				configVersion = config.getInt("Config Version");
+			} else {
+				Log.info("New Config File Created Successfully!");
+				config = new Config(plugin, "config.yml");
+				Variables.initizalizeVariables(this);
+			}
+		} else {
+			config = new Config(plugin, "config.yml");
+			Variables.initizalizeVariables(this);
+		}
+	}
+
+	private void loadUsers() {
+		try {
+			if (!usersFile.exists()) {
+				plugin.saveResource("users.yml", false);
+				Log.info("New Users File Created Successfully!");
+				return;
+			}
+			users.load(usersFile);
+		} catch (final Exception e) {
+			Log.severe("Error loading users file! Error: ");
+			e.printStackTrace();
+		}
+	}
+
+	public final void saveUser(final UUID uuid, final boolean save) {
+		final String id = uuid.toString();
+		final List<String> userList = users.getStringList("players");
+		if (save && userList.contains(id) || !save && !userList.contains(id))
+			return;
+		if (!save && userList.contains(id))
+			userList.remove(id);
+		if (save && !userList.contains(id))
+			userList.add(id);
+
+		users.set("players", userList);
+		try {
+			users.save(usersFile);
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public final FileConfiguration getConfig() {
+		return config;
+	}
+
+	public final YamlConfiguration getUserFile() {
+		return users;
+	}
+
+	public final int getConfigVersion() {
+		return configVersion;
+	}
+
+}
---
+++ PvPManager/src/main/java/me/NoChance/PvPManager/PvPManager.java
@@ -0,0 +1,137 @@
+package me.NoChance.PvPManager;
+
+import java.io.File;
+
+import me.NoChance.PvPManager.Commands.PM;
+import me.NoChance.PvPManager.Commands.PvP;
+import me.NoChance.PvPManager.Commands.PvPInfo;
+import me.NoChance.PvPManager.Commands.PvPList;
+import me.NoChance.PvPManager.Commands.PvPOverride;
+import me.NoChance.PvPManager.Commands.PvPStatus;
+import me.NoChance.PvPManager.Config.LogFile;
+import me.NoChance.PvPManager.Config.Messages;
+import me.NoChance.PvPManager.Config.Variables;
+import me.NoChance.PvPManager.Lib.CustomMetrics;
+import me.NoChance.PvPManager.Listeners.EntityListener;
+import me.NoChance.PvPManager.Listeners.PlayerListener;
+import me.NoChance.PvPManager.Listeners.WGListener;
+import me.NoChance.PvPManager.Managers.ConfigManager;
+import me.NoChance.PvPManager.Managers.DependencyManager;
+import me.NoChance.PvPManager.Managers.PlayerHandler;
+import me.NoChance.PvPManager.Updater.BukkitUpdater;
+import me.NoChance.PvPManager.Updater.SpigotUpdater;
+import me.NoChance.PvPManager.Updater.Updater;
+import me.NoChance.PvPManager.Updater.Updater.UpdateResult;
+import me.NoChance.PvPManager.Updater.Updater.UpdateType;
+import me.NoChance.PvPManager.Utils.Log;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public final class PvPManager extends JavaPlugin {
+
+	private ConfigManager configM;
+	private PlayerHandler playerHandler;
+	private Updater updater;
+	private LogFile log;
+	private DependencyManager dependencyManager;
+
+	@Override
+	public void onEnable() {
+		Log.setup(getLogger());
+		loadFiles();
+		dependencyManager = new DependencyManager(this);
+		playerHandler = new PlayerHandler(this);
+		startListeners();
+		getCommand("pvp").setExecutor(new PvP(playerHandler));
+		getCommand("pvpmanager").setExecutor(new PM(this));
+		getCommand("pvpoverride").setExecutor(new PvPOverride(playerHandler));
+		getCommand("pvpinfo").setExecutor(new PvPInfo(playerHandler));
+		getCommand("pvplist").setExecutor(new PvPList(playerHandler));
+		getCommand("pvpstatus").setExecutor(new PvPStatus(playerHandler));
+		startMetrics();
+		if (Variables.isUpdateCheck()) {
+			new BukkitRunnable() {
+				@Override
+				public void run() {
+					checkForUpdates();
+				}
+			}.runTaskTimer(this, 0, 360000);
+		}
+	}
+
+	@Override
+	public void onDisable() {
+		playerHandler.getTagTask().cancel();
+		for (final PvPlayer p : playerHandler.getPlayers().values()) {
+			if (p.isInCombat())
+				p.unTag();
+			playerHandler.savePvPState(p.getUUID(), p.hasPvPEnabled());
+		}
+		playerHandler.removeTeams();
+	}
+
+	private void loadFiles() {
+		this.configM = new ConfigManager(this);
+		Messages.setup(this);
+		if (Variables.isLogToFile())
+			log = new LogFile(new File(getDataFolder(), "pvplog.txt"));
+	}
+
+	private void startListeners() {
+		registerListener(new EntityListener(playerHandler));
+		registerListener(new PlayerListener(playerHandler));
+		if (dependencyManager.useWG())
+			registerListener(new WGListener(playerHandler));
+	}
+
+	private void startMetrics() {
+		new CustomMetrics(this);
+	}
+
+	private void checkForUpdates() {
+		Log.info("Checking for updates...");
+		if (Variables.getUpdateLocation().equalsIgnoreCase("Bukkit")) {
+			updater = new BukkitUpdater(this, 63773, UpdateType.VERSION_CHECK);
+		} else
+			updater = new SpigotUpdater(this, UpdateType.VERSION_CHECK);
+		if (updater.getResult() == UpdateResult.UPDATE_AVAILABLE) {
+			Messages.setNewVersion(updater.getLatestName());
+			Log.info("Update Available: " + Messages.getNewversion());
+			if (Variables.isAutoUpdate()) {
+				downloadUpdate();
+				Log.info("Version Downloaded To Your Update Folder");
+				return;
+			}
+			Variables.setUpdate(true);
+			Log.info("Link: http://dev.bukkit.org/bukkit-plugins/pvpmanager/");
+		} else
+			Log.info("No update found");
+	}
+
+	public boolean downloadUpdate() {
+		return updater.downloadFile();
+	}
+
+	public void registerListener(final Listener listener) {
+		this.getServer().getPluginManager().registerEvents(listener, this);
+	}
+
+	public ConfigManager getConfigM() {
+		return configM;
+	}
+
+	public PlayerHandler getPlayerHandler() {
+		return playerHandler;
+	}
+
+	public LogFile getLog() {
+		return log;
+	}
+
+	public DependencyManager getDependencyManager() {
+		return dependencyManager;
+	}
+
+}
