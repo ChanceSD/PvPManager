8ad86a418196e2bdedc6b0a08f64e71486625773
NoChanceSD <nochance20@gmail.com>
2015-07-23 11:56:36 +0100

    Truced factions can't attack each other Fixes #38

--- PvPManager/src/main/java/me/NoChance/PvPManager/Dependencies/Factions.java
+++ PvPManager/src/main/java/me/NoChance/PvPManager/Dependencies/Factions.java
@@ -1,11 +1,11 @@
 package me.NoChance.PvPManager.Dependencies;
 
-import me.NoChance.PvPManager.PvPlugin;
-
 import org.bukkit.entity.Player;
 
 import com.massivecraft.factions.Rel;
 import com.massivecraft.factions.entity.MPlayer;
+
+import me.NoChance.PvPManager.PvPlugin;
 
 
 public class Factions implements PvPlugin {
@@ -14,7 +14,7 @@ public class Factions implements PvPlugin {
 	public final boolean canAttack(final Player attacker, final Player defender) {
 		final MPlayer fAttacker = MPlayer.get(attacker);
 		final MPlayer fDefender = MPlayer.get(defender);
-		return !fAttacker.getRelationTo(fDefender).isAtLeast(Rel.ALLY);
+		return !fAttacker.getRelationTo(fDefender).isAtLeast(Rel.TRUCE);
 	}
 
 }
