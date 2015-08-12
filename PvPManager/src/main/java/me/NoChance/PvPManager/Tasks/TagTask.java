2feee41ad99d28145d52ccc971646b455950f7bb
NoChanceSD <nochance20@gmail.com>
2015-08-12 22:23:07 +0100

    Trying to synchronize this properly

--- PvPManager/src/main/java/me/NoChance/PvPManager/Tasks/TagTask.java
+++ PvPManager/src/main/java/me/NoChance/PvPManager/Tasks/TagTask.java
@@ -1,33 +1,49 @@
 package me.NoChance.PvPManager.Tasks;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
+
+import org.bukkit.scheduler.BukkitRunnable;
 
 import me.NoChance.PvPManager.PvPlayer;
 import me.NoChance.PvPManager.Config.Variables;
 import me.NoChance.PvPManager.Utils.CombatUtils;
 
-import org.bukkit.scheduler.BukkitRunnable;
-
 public class TagTask extends BukkitRunnable {
 
 	private final int time = Variables.getTimeInCombat();
-	private final HashSet<PvPlayer> tagged = new HashSet<>();
+	private final Set<PvPlayer> tagged = Collections.synchronizedSet(new HashSet<PvPlayer>());
 
 	@Override
 	public final void run() {
-		final Iterator<PvPlayer> iterator = getTagged().iterator();
-		while (iterator.hasNext()) {
-			final PvPlayer p = iterator.next();
-			if (CombatUtils.hasTimePassed(p.getTaggedTime(), time)) {
-				p.unTag();
-				iterator.remove();
+		synchronized (tagged) {
+			final Iterator<PvPlayer> iterator = tagged.iterator();
+			while (iterator.hasNext()) {
+				final PvPlayer p = iterator.next();
+				if (CombatUtils.hasTimePassed(p.getTaggedTime(), time)) {
+					p.unTag();
+					iterator.remove();
+				}
 			}
 		}
 	}
 
-	public final synchronized HashSet<PvPlayer> getTagged() {
-		return tagged;
+	@Override
+	public synchronized void cancel() throws IllegalStateException {
+		super.cancel();
+		tagged.clear();
+	}
+
+	public void addTagged(final PvPlayer p) {
+		tagged.add(p);
+	}
+
+	public void untag(final PvPlayer p) {
+		tagged.remove(p);
+		if (p.isInCombat())
+			p.unTag();
 	}
 
 }
