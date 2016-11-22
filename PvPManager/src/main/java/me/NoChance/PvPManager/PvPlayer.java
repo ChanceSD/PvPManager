73e7e5f3e26cc3d7d208c01cfdb9a6c9b42f4916
NoChanceSD <nochance20@gmail.com>
2016-11-22 01:01:57 +0000

    Glowing will always work from now on so make something decent to check version

--- PvPManager/src/main/java/me/NoChance/PvPManager/PvPlayer.java
+++ PvPManager/src/main/java/me/NoChance/PvPManager/PvPlayer.java
@@ -139,7 +139,7 @@ public class PvPlayer extends EcoPlayer {
 		if (Settings.isUseNameTag()) {
 			teamProfile.setInCombat();
 		}
-		if (Settings.isGlowingInCombat() && (Bukkit.getBukkitVersion().startsWith("1.9") || Bukkit.getBukkitVersion().startsWith("1.10"))) {
+		if (Settings.isGlowingInCombat() && Settings.getMinecraftVersion() > 1.9) {
 			getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, Settings.getTimeInCombat() * 20, 1));
 		}
 
