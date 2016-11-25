e54e97640277583e4da470919e706140c00393d6
NoChanceSD <nochance20@gmail.com>
2016-11-25 19:16:59 +0000

    That way of version checking wasn't going to work, need some proper checking

--- PvPManager/src/main/java/me/NoChance/PvPManager/Utils/CombatUtils.java
+++ PvPManager/src/main/java/me/NoChance/PvPManager/Utils/CombatUtils.java
@@ -97,4 +97,26 @@ public final class CombatUtils {
 		}
 		return contains;
 	}
+
+	public static final boolean isVersionSuperior(final String v1, final String v2) {
+		if (v1.equals(v2))
+			return false;
+
+		final String[] v1Array = v1.split("\\.");
+		final String[] v2Array = v2.split("\\.");
+		final int length = Math.max(v2Array.length, v1Array.length);
+		try {
+			for (int i = 0; i < length; i++) {
+				final int x = i < v2Array.length ? Integer.parseInt(v2Array[i]) : 0;
+				final int y = i < v1Array.length ? Integer.parseInt(v1Array[i]) : 0;
+				if (y > x)
+					return true;
+				if (y < x)
+					return false;
+			}
+		} catch (final NumberFormatException ex) {
+			Log.warning("Error reading version number!");
+		}
+		return true;
+	}
 }
