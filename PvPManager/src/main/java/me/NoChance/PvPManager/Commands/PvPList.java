bfa2b73a5dbd366e22fb4a73e7a0913952aa17f2
NoChanceSD <nochance20@gmail.com>
2018-07-21 23:01:27 +0100

    Hide vanished players from /pvplist

--- PvPManager/src/main/java/me/NoChance/PvPManager/Commands/PvPList.java
+++ PvPManager/src/main/java/me/NoChance/PvPManager/Commands/PvPList.java
@@ -1,13 +1,14 @@
 package me.NoChance.PvPManager.Commands;
 
-import me.NoChance.PvPManager.PvPlayer;
-import me.NoChance.PvPManager.Managers.PlayerHandler;
-import me.NoChance.PvPManager.Settings.Messages;
-
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import me.NoChance.PvPManager.PvPlayer;
+import me.NoChance.PvPManager.Managers.PlayerHandler;
+import me.NoChance.PvPManager.Settings.Messages;
 
 public class PvPList implements CommandExecutor {
 
@@ -20,14 +21,14 @@ public class PvPList implements CommandExecutor {
 	@Override
 	public final boolean onCommand(final CommandSender sender, final Command cmd, final String label, final String[] args) {
 		sender.sendMessage(ChatColor.GOLD + "**** Players With PvP Enabled ****");
-		sender.sendMessage(ChatColor.DARK_GRAY + pvpList());
+		sender.sendMessage(ChatColor.DARK_GRAY + (sender instanceof Player ? pvpList((Player) sender, false) : pvpList(null, true)));
 		return true;
 	}
 
-	private String pvpList() {
+	private String pvpList(final Player sender, final boolean console) {
 		final StringBuilder list = new StringBuilder();
 		for (final PvPlayer p : ph.getPlayers().values()) {
-			if (p.hasPvPEnabled()) {
+			if (p.hasPvPEnabled() && (console || sender.canSee(p.getPlayer()))) {
 				list.append(p.getName()).append(", ");
 			}
 		}
