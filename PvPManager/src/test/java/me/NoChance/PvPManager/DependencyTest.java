41e9f4cbbfffef8f2abd2215a20c1f1ddbf7dc87
NoChanceSD <nochance20@gmail.com>
2019-12-14 03:09:11 +0000

    Fix error reading WorldGuard version

--- PvPManager/src/test/java/me/NoChance/PvPManager/DependencyTest.java
+++ PvPManager/src/test/java/me/NoChance/PvPManager/DependencyTest.java
@@ -1,5 +1,6 @@
 package me.NoChance.PvPManager;
 
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -14,6 +15,7 @@ import org.mockito.Matchers;
 import org.powermock.api.mockito.PowerMockito;
 
 import me.NoChance.PvPManager.Managers.DependencyManager;
+import me.NoChance.PvPManager.Utils.CombatUtils;
 
 public class DependencyTest {
 
@@ -32,6 +34,14 @@ public class DependencyTest {
 		when(plugin.getDescription()).thenReturn(new PluginDescriptionFile("Plugin", "1.0", "plugin"));
 		when(server.getPluginManager().getPlugin(Matchers.anyString())).thenReturn(plugin);
 		new DependencyManager();
+	}
+
+	@Test
+	public void versionTags() {
+		final String v1 = CombatUtils.stripTags("1.0.2-SNAPSHOT");
+		final String v2 = CombatUtils.stripTags("1.0.2;1994-9adac4f");
+		assertTrue(v1.equals("1.0.2"));
+		assertTrue(v2.equals("1.0.2"));
 	}
 
 	@AfterClass
