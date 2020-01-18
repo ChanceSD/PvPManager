07b0e7b3cb85350271350501fa94806a5378434a
NoChanceSD <nochance20@gmail.com>
2020-01-18 12:39:04 +0000

    Force remove glow effect when untagged

--- PvPManager/src/main/java/me/NoChance/PvPManager/PvPlayer.java
+++ PvPManager/src/main/java/me/NoChance/PvPManager/PvPlayer.java
@@ -153,6 +153,9 @@ public class PvPlayer extends EcoPlayer {
 			if (Settings.isUseNameTag()) {
 				teamProfile.restoreTeam();
 			}
+			if (Settings.isGlowingInCombat() && CombatUtils.isVersionAtLeast(Settings.getMinecraftVersion(), "1.9")) {
+				getPlayer().removePotionEffect(PotionEffectType.GLOWING); // effect should pass by itself but now players can get untagged before tag expires
+			}
 
 			message(Messages.getOutOfCombat());
 			sendActionBar(Messages.getOutOfCombat());
