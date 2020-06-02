848a00c9a9f77d493f972b6a270ff2b09103ded9
ChanceSD <NoChanceSD@users.noreply.github.com>
2020-06-02 22:01:50 +0100

    Add basic tag events, might be improved later #207

---
+++ PvPManager/src/main/java/me/NoChance/PvPManager/Events/PlayerTagEvent.java
@@ -0,0 +1,48 @@
+package me.NoChance.PvPManager.Events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import me.NoChance.PvPManager.PvPlayer;
+
+public final class PlayerTagEvent extends Event implements Cancellable {
+
+	private static final HandlerList handlers = new HandlerList();
+	private boolean cancelled;
+	private final Player player;
+	private final PvPlayer pvplayer;
+
+	public PlayerTagEvent(final Player player, final PvPlayer pvplayer) {
+		this.player = player;
+		this.pvplayer = pvplayer;
+	}
+
+	public final Player getPlayer() {
+		return player;
+	}
+
+	public final PvPlayer getPvPlayer() {
+		return pvplayer;
+	}
+
+	@Override
+	public boolean isCancelled() {
+		return cancelled;
+	}
+
+	@Override
+	public void setCancelled(final boolean cancel) {
+		cancelled = cancel;
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+}
