f4906c97568443f300a40547337bc6a3fb103d4b
ChanceSD <5256215+ChanceSD@users.noreply.github.com>
2022-04-23 20:15:52 +0100

    Make sure it's MC 1.9+ for chorus fruit

--- PvPManager/src/main/java/me/NoChance/PvPManager/Listeners/PlayerListener.java
+++ PvPManager/src/main/java/me/NoChance/PvPManager/Listeners/PlayerListener.java
@@ -226,7 +226,8 @@ public class PlayerListener implements Listener {
 			if (event.getCause().equals(TeleportCause.ENDER_PEARL) && Settings.isBlockEnderPearl()) {
 				event.setCancelled(true);
 				player.message(Messages.getEnderpearlBlockedIncombat());
-			} else if (event.getCause().equals(TeleportCause.CHORUS_FRUIT) && Settings.isBlockChorusFruit()) {
+			} else if (CombatUtils.isVersionAtLeast(Settings.getMinecraftVersion(), "1.9") && event.getCause().equals(TeleportCause.CHORUS_FRUIT)
+			        && Settings.isBlockChorusFruit()) {
 				event.setCancelled(true);
 				player.message(Messages.getChorusBlockedInCombat());
 			} else if (event.getCause().equals(TeleportCause.COMMAND) && Settings.isBlockTeleport()) {
