78d3a6991e4cf75185344176c26bcc8ab30b1c19
ChanceSD <5256215+ChanceSD@users.noreply.github.com>
2023-04-11 01:20:50 +0100

    Actually allow users to use French, Polish, etc lol

--- pvpmanager/src/main/java/me/NoChance/PvPManager/Settings/Locale.java
+++ pvpmanager/src/main/java/me/NoChance/PvPManager/Settings/Locale.java
@@ -5,14 +5,17 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public enum Locale {
-	EN("messages.properties"),
-	RU("messages_ru.properties"),
-	ZH("messages_zh.properties"),
-	ZH_TW("messages_zh_TW.properties"),
-	ES("messages_es.properties"),
+	BG("messages_bg.properties"),
 	DE("messages_de.properties"),
+	ES("messages_es.properties"),
+	FR("messages_fr.properties"),
+	IT("messages_it.properties"),
+	PL("messages_pl.properties"),
 	PT_BR("messages_pt_BR.properties"),
-	IT("messages_it.properties");
+	RU("messages_ru.properties"),
+	ZH_TW("messages_zh_TW.properties"),
+	ZH("messages_zh.properties"),
+	EN("messages.properties");
 
 	private final String fileName;
 
