JVM-Squad/autoscan-PvPManager

Finally discard old package

pvpmanager/src/main/java/me/chancesd/pvpmanager/tasks/PvPToggleFeeTask.java
@@ -1,19 +1,19 @@
 package me.chancesd.pvpmanager.tasks;
 
-import me.NoChance.PvPManager.PvPlayer;
-import me.chancesd.pvpmanager.manager.PlayerHandler;
+import me.chancesd.pvpmanager.manager.PlayerManager;
+import me.chancesd.pvpmanager.player.CombatPlayer;
 
 public class PvPToggleFeeTask implements Runnable {
 
-	private final PlayerHandler ph;
+	private final PlayerManager ph;
 
-	public PvPToggleFeeTask(final PlayerHandler ph) {
+	public PvPToggleFeeTask(final PlayerManager ph) {
 		this.ph = ph;
 	}
 
 	@Override
 	public final void run() {
-		for (final PvPlayer p : ph.getPlayers().values()) {
+		for (final CombatPlayer p : ph.getPlayers().values()) {
 			if (!p.hasPvPEnabled()) {
 				p.applyPvPDisabledFee();
 			}

ChanceSD/PvPManager

pvpmanager/src/main/java/me/chancesd/pvpmanager/tasks/PvPToggleFeeTask.java
@@ -1,20 +1,20 @@
 package me.chancesd.pvpmanager.tasks;
 
-import me.NoChance.PvPManager.PvPlayer;
-import me.chancesd.pvpmanager.manager.PlayerHandler;
+import me.chancesd.pvpmanager.manager.PlayerManager;
+import me.chancesd.pvpmanager.player.CombatPlayer;
 import me.chancesd.pvpmanager.setting.Messages;
 
 public class PvPToggleFeeTask implements Runnable {
 
-	private final PlayerHandler ph;
+	private final PlayerManager ph;
 
-	public PvPToggleFeeTask(final PlayerHandler ph) {
+	public PvPToggleFeeTask(final PlayerManager ph) {
 		this.ph = ph;
 	}
 
 	@Override
 	public final void run() {
-		for (final PvPlayer combatPlayer : ph.getPlayers().values()) {
+		for (final CombatPlayer combatPlayer : ph.getPlayers().values()) {
 			if (!combatPlayer.hasPvPEnabled() && !combatPlayer.applyPvPDisabledFee()) {
 				combatPlayer.setPvP(true);
 				combatPlayer.message(Messages.getPvpFeeNotEnough());