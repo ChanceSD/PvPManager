JVM-Squad/autoscan-PvPManager

Move to new package

pvpmanager/src/main/java/me/chancesd/pvpmanager/listener/MoveListener1_9.java
@@ -1,0 +1,75 @@
+package me.chancesd.pvpmanager.listener;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.util.Vector;
+import org.jetbrains.annotations.NotNull;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
+import me.NoChance.PvPManager.PvPlayer;
+import me.chancesd.pvpmanager.manager.DependencyManager;
+import me.chancesd.pvpmanager.manager.PlayerHandler;
+import me.chancesd.pvpmanager.setting.Messages;
+import me.chancesd.pvpmanager.tasks.RegionCheckTask;
+import me.chancesd.pvpmanager.utils.CombatUtils;
+import me.chancesd.pvpmanager.utils.ScheduleUtils;
+
+public class MoveListener1_9 implements Listener {
+
+	private final PlayerHandler ph;
+	private final Cache<UUID, Player> cache = CacheBuilder.newBuilder().weakValues().expireAfterWrite(1, TimeUnit.SECONDS).build();
+	private final DependencyManager dependencyManager;
+
+	public MoveListener1_9(final PlayerHandler ph) {
+		this.ph = ph;
+		this.dependencyManager = ph.getPlugin().getDependencyManager();
+		final RegionCheckTask regionCheckTask = new RegionCheckTask(ph, dependencyManager);
+		Bukkit.getPluginManager().registerEvents(regionCheckTask, ph.getPlugin());
+		ScheduleUtils.runPlatformTaskTimer(regionCheckTask, 20, 20);
+	}
+
+	@EventHandler(ignoreCancelled = true)
+	public final void onPlayerMove(final PlayerMoveEvent event) {
+		final Location locTo = event.getTo();
+		final Location locFrom = event.getFrom();
+		if (locFrom.getBlockX() == locTo.getBlockX() && locFrom.getBlockZ() == locTo.getBlockZ() && locFrom.getBlockY() == locTo.getBlockY())
+			return;
+
+		final Player player = event.getPlayer();
+		final PvPlayer pvplayer = ph.get(player);
+		if (!pvplayer.isInCombat())
+			return;
+
+		if (!dependencyManager.canAttackAt(null, locTo) && dependencyManager.canAttackAt(null, locFrom)) {
+			final Vector newVel = locFrom.toVector().subtract(locTo.toVector());
+			newVel.setY(0).normalize().multiply(1.6).setY(0.5);
+			CombatUtils.checkGlide(player);
+			player.setVelocity(sanitizeVector(newVel));
+			if (!cache.asMap().containsKey(player.getUniqueId())) {
+				pvplayer.message(Messages.getPushbackWarning());
+				locFrom.getWorld().playEffect(player.getEyeLocation(), Effect.SMOKE, 3);
+				cache.put(player.getUniqueId(), player);
+			}
+		}
+	}
+
+	@NotNull
+	private Vector sanitizeVector(@NotNull final Vector vel) {
+		if (Double.isNaN(vel.getX()))
+			vel.setX(0);
+		if (Double.isNaN(vel.getZ()))
+			vel.setZ(0);
+		return vel;
+	}
+
+}

pvpmanager/src/main/java/me/chancesd/pvpmanager/manager/DisplayManager.java
@@ -1,0 +1,69 @@
+package me.chancesd.pvpmanager.manager;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.boss.BossBar;
+import org.jetbrains.annotations.NotNull;
+
+import me.NoChance.PvPManager.PvPManager;
+import me.NoChance.PvPManager.PvPlayer;
+import me.NoChance.PvPManager.Player.display.ProgressBar;
+import me.chancesd.pvpmanager.setting.Settings;
+import me.chancesd.pvpmanager.utils.ChatUtils;
+import me.chancesd.sdutils.utils.Log;
+
+public class DisplayManager {
+
+	private final Map<PvPlayer, ProgressBar> actionBars = new HashMap<>();
+	private final Map<PvPlayer, BossBar> bossBars = new ConcurrentHashMap<>();
+	private final PvPManager plugin;
+
+	public DisplayManager(final PvPManager plugin) {
+		this.plugin = plugin;
+	}
+
+	public void updateBossbar(final PvPlayer player, final double timePassed, final int totalTime) {
+		final BossBar bossBar = bossBars.computeIfAbsent(player, this::setupBossbar);
+		final String message = Settings.getBossBarMessage().replace("<time>", Long.toString(totalTime - Math.round(timePassed)));
+		final String placeHolderMessage = ChatUtils.setPlaceholders(player.getPlayer(), message);
+		if (!bossBar.getTitle().equals(placeHolderMessage))
+			bossBar.setTitle(placeHolderMessage);
+		bossBar.setProgress((totalTime - timePassed) / totalTime);
+	}
+
+	private BossBar setupBossbar(final PvPlayer player) {
+		final BossBar bossBar = Bukkit.createBossBar("", Settings.getBossBarColor(), Settings.getBossBarStyle());
+		bossBar.addPlayer(player.getPlayer());
+		return bossBar;
+	}
+
+	public void discardBossbar(final PvPlayer player) {
+		final BossBar bossBar = bossBars.get(player);
+		if (bossBar == null) {
+			Log.debug("Tried to discard bossbar that didn't exist"); // display task might not have created one yet
+			return;
+		}
+		bossBar.removePlayer(player.getPlayer());
+		bossBars.remove(player);
+	}
+
+	private ProgressBar setupProgressBar(final long time, final int goal) {
+		return new ProgressBar(Settings.getActionBarMessage(), Settings.getActionBarBars(), goal, Settings.getActionBarSymbol(), time);
+	}
+
+	public void showProgress(final PvPlayer p, final double timePassed, final int goal) {
+		final long timePassedRounded = Math.round(timePassed);
+		final ProgressBar progressBar = actionBars.computeIfAbsent(p, x -> setupProgressBar(timePassedRounded, goal));
+		progressBar.setProgress(timePassedRounded).setGoal(goal).calculate();
+		p.sendActionBar(ChatUtils.setPlaceholders(p.getPlayer(), progressBar.getMessage()));
+	}
+
+	@NotNull
+	public PvPManager getPlugin() {
+		return plugin;
+	}
+
+}

pvpmanager/src/main/java/me/chancesd/pvpmanager/tasks/TagTask.java
@@ -1,0 +1,80 @@
+package me.chancesd.pvpmanager.tasks;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
+
+import me.NoChance.PvPManager.PvPlayer;
+import me.chancesd.pvpmanager.manager.DisplayManager;
+import me.chancesd.pvpmanager.setting.Settings;
+import me.chancesd.sdutils.utils.Log;
+
+public class TagTask extends TimerTask {
+
+	private final Timer timer;
+	private final Set<PvPlayer> tagged = ConcurrentHashMap.newKeySet();
+	private final DisplayManager display;
+
+	public TagTask(final DisplayManager display) {
+		this.display = display;
+		this.timer = new Timer("PvPManager Display Thread");
+		timer.scheduleAtFixedRate(this, 1000, 100);
+	}
+
+	@Override
+	public final void run() {
+		try {
+			final Iterator<PvPlayer> iterator = tagged.iterator();
+			while (iterator.hasNext()) {
+				final PvPlayer p = iterator.next();
+				final long currentTime = System.currentTimeMillis();
+				if (currentTime >= p.getUntagTime()) {
+					p.unTag();
+					display.discardBossbar(p);
+					iterator.remove();
+					continue;
+				}
+				final double timePassed = (currentTime - p.getTaggedTime()) / 1000D;
+				final int totalTagTimeInSeconds = (int) (p.getTotalTagTime() / 1000);
+				if (Settings.isActionBarEnabled()) {
+					display.showProgress(p, timePassed, totalTagTimeInSeconds);
+				}
+				if (Settings.isBossBarEnabled()) {
+					display.updateBossbar(p, timePassed, totalTagTimeInSeconds);
+				}
+			}
+		} catch (final Exception e) {
+			Log.severe("Error in tag task", e);
+			throw e;
+		}
+	}
+
+	@Override
+	public final boolean cancel() {
+		for (final PvPlayer pvPlayer : tagged)
+			if (pvPlayer.isInCombat()) {
+				display.discardBossbar(pvPlayer);
+				pvPlayer.unTag();
+			}
+		tagged.clear();
+		super.cancel();
+		timer.cancel();
+		return false;
+	}
+
+	public final void addTagged(final PvPlayer p) {
+		tagged.add(p);
+	}
+
+	public final void untag(final PvPlayer p) {
+		display.discardBossbar(p);
+		tagged.remove(p);
+	}
+
+	public Set<PvPlayer> getTaggedPlayers() {
+		return tagged;
+	}
+
+}

ChanceSD/PvPManager

pvpmanager/src/main/java/me/chancesd/pvpmanager/listener/MoveListener1_9.java
@@ -1,0 +1,75 @@
+package me.chancesd.pvpmanager.listener;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.util.Vector;
+import org.jetbrains.annotations.NotNull;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
+import me.NoChance.PvPManager.PvPlayer;
+import me.chancesd.pvpmanager.manager.DependencyManager;
+import me.chancesd.pvpmanager.manager.PlayerHandler;
+import me.chancesd.pvpmanager.setting.Messages;
+import me.chancesd.pvpmanager.tasks.RegionCheckTask;
+import me.chancesd.pvpmanager.utils.CombatUtils;
+import me.chancesd.pvpmanager.utils.ScheduleUtils;
+
+public class MoveListener1_9 implements Listener {
+
+	private final PlayerHandler playerManager;
+	private final DependencyManager depManager;
+	private final Cache<UUID, Player> cache = CacheBuilder.newBuilder().weakValues().expireAfterWrite(1, TimeUnit.SECONDS).build();
+
+	public MoveListener1_9(final PlayerHandler ph, final DependencyManager depManager) {
+		this.ph = ph;
+		this.dependencyManager = ph.getPlugin().getDependencyManager();
+		final RegionCheckTask regionCheckTask = new RegionCheckTask(ph, dependencyManager);
+		Bukkit.getPluginManager().registerEvents(regionCheckTask, ph.getPlugin());
+		ScheduleUtils.runPlatformTaskTimer(regionCheckTask, 20, 20);
+	}
+
+	@EventHandler(ignoreCancelled = true)
+	public final void onPlayerMove(final PlayerMoveEvent event) {
+		final Location locTo = event.getTo();
+		final Location locFrom = event.getFrom();
+		if (locFrom.getBlockX() == locTo.getBlockX() && locFrom.getBlockZ() == locTo.getBlockZ() && locFrom.getBlockY() == locTo.getBlockY())
+			return;
+
+		final Player player = event.getPlayer();
+		final PvPlayer pvplayer = playerManager.get(player);
+		if (!pvplayer.isInCombat())
+			return;
+
+		if (!dependencyManager.canAttackAt(null, locTo) && dependencyManager.canAttackAt(null, locFrom)) {
+			final Vector newVel = locFrom.toVector().subtract(locTo.toVector());
+			newVel.setY(0).normalize().multiply(1.6).setY(0.5);
+			CombatUtils.checkGlide(player);
+			player.setVelocity(sanitizeVector(newVel));
+			if (!cache.asMap().containsKey(player.getUniqueId())) {
+				pvplayer.message(Messages.getPushbackWarning());
+				locFrom.getWorld().playEffect(player.getEyeLocation(), Effect.SMOKE, 3);
+				cache.put(player.getUniqueId(), player);
+			}
+		}
+	}
+
+	@NotNull
+	private Vector sanitizeVector(@NotNull final Vector vel) {
+		if (Double.isNaN(vel.getX()))
+			vel.setX(0);
+		if (Double.isNaN(vel.getZ()))
+			vel.setZ(0);
+		return vel;
+	}
+
+}

pvpmanager/src/main/java/me/chancesd/pvpmanager/manager/DisplayManager.java
@@ -1,0 +1,71 @@
+package me.chancesd.pvpmanager.manager;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.boss.BossBar;
+import org.jetbrains.annotations.NotNull;
+
+import me.NoChance.PvPManager.PvPManager;
+import me.NoChance.PvPManager.PvPlayer;
+import me.NoChance.PvPManager.Player.display.ProgressBar;
+import me.chancesd.pvpmanager.setting.Settings;
+import me.chancesd.pvpmanager.utils.ChatUtils;
+import me.chancesd.sdutils.utils.Log;
+
+public class DisplayManager {
+
+	private final Map<PvPlayer, ProgressBar> actionBars = new HashMap<>();
+	private final Map<PvPlayer, BossBar> bossBars = new ConcurrentHashMap<>();
+	private final PvPManager plugin;
+
+	public DisplayManager(final PvPManager plugin) {
+		this.plugin = plugin;
+	}
+
+	public void updateBossbar(final PvPlayer player, final double timePassed, final int totalTime) {
+		final BossBar bossBar = bossBars.computeIfAbsent(player, this::setupBossbar);
+		final String message = Settings.getBossBarMessage().replace("<time>", Long.toString(totalTime - Math.round(timePassed)));
+		final String placeHolderMessage = ChatUtils.setPlaceholders(player.getPlayer(), message);
+		if (!bossBar.getTitle().equals(placeHolderMessage))
+			bossBar.setTitle(placeHolderMessage);
+		bossBar.setProgress((totalTime - timePassed) / totalTime);
+	}
+
+	private BossBar setupBossbar(final PvPlayer player) {
+		final BossBar bossBar = Bukkit.createBossBar("", Settings.getBossBarColor(), Settings.getBossBarStyle());
+		bossBar.addPlayer(player.getPlayer());
+		return bossBar;
+	}
+
+	public void discardPlayer(final PvPlayer player) {
+		if (actionBars.remove(player) == null)
+			Log.debug("Tried to discard action bar that didn't exist");
+		final BossBar bossBar = bossBars.get(player);
+		if (bossBar == null) {
+			Log.debug("Tried to discard bossbar that didn't exist"); // display task might not have created one yet
+			return;
+		}
+		bossBar.removePlayer(player.getPlayer());
+		bossBars.remove(player);
+	}
+
+	private ProgressBar setupProgressBar(final long time, final int goal) {
+		return new ProgressBar(Settings.getActionBarMessage(), Settings.getActionBarBars(), goal, Settings.getActionBarSymbol(), time);
+	}
+
+	public void showProgress(final PvPlayer p, final double timePassed, final int goal) {
+		final long timePassedRounded = Math.round(timePassed);
+		final ProgressBar progressBar = actionBars.computeIfAbsent(p, x -> setupProgressBar(timePassedRounded, goal));
+		progressBar.setProgress(timePassedRounded).setGoal(goal).calculate();
+		p.sendActionBar(ChatUtils.setPlaceholders(p.getPlayer(), progressBar.getMessage()));
+	}
+
+	@NotNull
+	public PvPManager getPlugin() {
+		return plugin;
+	}
+
+}

pvpmanager/src/main/java/me/chancesd/pvpmanager/tasks/TagTask.java
@@ -1,0 +1,82 @@
+package me.chancesd.pvpmanager.tasks;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
+
+import me.NoChance.PvPManager.PvPlayer;
+import me.chancesd.pvpmanager.manager.DisplayManager;
+import me.chancesd.pvpmanager.setting.Settings;
+import me.chancesd.sdutils.utils.Log;
+
+public class TagTask extends TimerTask {
+
+	private final Timer timer;
+	private final Set<PvPlayer> tagged = ConcurrentHashMap.newKeySet();
+	private final DisplayManager display;
+
+	public TagTask(final DisplayManager display) {
+		this.display = display;
+		this.timer = new Timer("PvPManager Display Thread");
+		timer.scheduleAtFixedRate(this, 1000, 100);
+	}
+
+	@Override
+	public final void run() {
+		try {
+			final Iterator<PvPlayer> iterator = tagged.iterator();
+			while (iterator.hasNext()) {
+				final PvPlayer p = iterator.next();
+				final long currentTime = System.currentTimeMillis();
+				if (currentTime >= p.getUntagTime()) {
+					p.unTag();
+					display.discardPlayer(p);
+					iterator.remove();
+					continue;
+				}
+				final double timePassed = (currentTime - p.getTaggedTime()) / 1000D;
+				final int totalTagTimeInSeconds = (int) (p.getTotalTagTime() / 1000);
+				if (Settings.isActionBarEnabled()) {
+					display.showProgress(p, timePassed, totalTagTimeInSeconds);
+				}
+				if (Settings.isBossBarEnabled()) {
+					display.updateBossbar(p, timePassed, totalTagTimeInSeconds);
+				}
+			}
+		} catch (final Exception e) {
+			Log.severe("Error in tag task", e);
+			e.printStackTrace();
+			Thread.currentThread().dumpStack();
+			throw e;
+		}
+	}
+
+	@Override
+	public final boolean cancel() {
+		for (final PvPlayer pvPlayer : tagged)
+			if (pvPlayer.isInCombat()) {
+				display.discardPlayer(pvPlayer);
+				pvPlayer.unTag();
+			}
+		tagged.clear();
+		super.cancel();
+		timer.cancel();
+		return false;
+	}
+
+	public final void addTagged(final PvPlayer p) {
+		tagged.add(p);
+	}
+
+	public final void untag(final PvPlayer p) {
+		display.discardPlayer(p);
+		tagged.remove(p);
+	}
+
+	public Set<PvPlayer> getTaggedPlayers() {
+		return tagged;
+	}
+
+}