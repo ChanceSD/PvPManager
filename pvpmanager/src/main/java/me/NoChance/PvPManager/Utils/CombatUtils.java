JVM-Squad/autoscan-PvPManager

Add option for XP steal, closes #219

pvpmanager/src/main/java/me/NoChance/PvPManager/Utils/CombatUtils.java
@@ -195,7 +195,7 @@ public final class CombatUtils {
 	}
 
 	public static boolean hasHarmfulPotion(final AreaEffectCloud areaCloud) {
-		if (isVersionAtLeast(Settings.getMinecraftVersion(), "1.20")) {
+		if (MCVersion.isAtLeast(MCVersion.V1_20)) {
 			final PotionType basePotionType = areaCloud.getBasePotionType();
 			if (basePotionType == null)
 				return false;

ChanceSD/PvPManager

pvpmanager/src/main/java/me/NoChance/PvPManager/Utils/CombatUtils.java
@@ -209,7 +209,7 @@ public final class CombatUtils {
 	}
 
 	public static boolean hasHarmfulPotion(final AreaEffectCloud areaCloud) {
-		if (isVersionAtLeast(Settings.getMinecraftVersion(), "1.20.2")) {
+		if (MCVersion.isAtLeast(MCVersion.V1_20_2)) {
 			final PotionType basePotionType = areaCloud.getBasePotionType();
 			if (basePotionType == null)
 				return false;