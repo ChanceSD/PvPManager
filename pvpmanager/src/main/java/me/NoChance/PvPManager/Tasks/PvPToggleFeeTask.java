a54ab8d257af8930770f368a48979cfe8c05c74d
ChanceSD <5256215+ChanceSD@users.noreply.github.com>
2025-01-26 00:50:49 +0000

    Log to console when applying pvp off fee

--- pvpmanager/src/main/java/me/NoChance/PvPManager/Tasks/PvPToggleFeeTask.java
+++ pvpmanager/src/main/java/me/NoChance/PvPManager/Tasks/PvPToggleFeeTask.java
@@ -1,9 +1,13 @@
 package me.NoChance.PvPManager.Tasks;
 
+import org.bukkit.ChatColor;
+
 import me.NoChance.PvPManager.PvPlayer;
 import me.NoChance.PvPManager.Managers.PlayerHandler;
 import me.NoChance.PvPManager.Settings.Messages;
+import me.NoChance.PvPManager.Settings.Settings;
 import me.chancesd.pvpmanager.utils.ScheduleUtils;
+import me.chancesd.sdutils.utils.Log;
 
 public class PvPToggleFeeTask implements Runnable {
 
@@ -15,12 +19,15 @@ public class PvPToggleFeeTask implements Runnable {
 
 	@Override
 	public final void run() {
+		int i = 0;
 		for (final PvPlayer combatPlayer : ph.getPlayers().values()) {
 			if (!combatPlayer.hasPvPEnabled() && !combatPlayer.applyPvPDisabledFee()) {
 				ScheduleUtils.ensureMainThread(() -> combatPlayer.setPvP(true), combatPlayer.getPlayer());
 				combatPlayer.message(Messages.getPvpFeeNotEnough());
+				i++;
 			}
 		}
+		Log.infoColor(ChatColor.GREEN + "Applied the PvP disabled fee of " + Settings.getPvPDisabledFee() + " to " + i + " players.");
 	}
 
 }
