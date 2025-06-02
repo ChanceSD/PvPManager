639bcb4c364f4ee03abe59784b8b6f06d1fbe187
ChanceSD <5256215+ChanceSD@users.noreply.github.com>
2025-06-02 13:58:17 +0100

    Fix Towny hook

--- pvpmanager/src/main/java/me/NoChance/PvPManager/Dependencies/Hooks/TownyHook.java
+++ pvpmanager/src/main/java/me/NoChance/PvPManager/Dependencies/Hooks/TownyHook.java
@@ -1,11 +1,15 @@
 package me.NoChance.PvPManager.Dependencies.Hooks;
 
+import java.util.UUID;
+
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import com.palmergames.bukkit.towny.TownyAPI;
+import com.palmergames.bukkit.towny.object.Nation;
 import com.palmergames.bukkit.towny.object.Resident;
 import com.palmergames.bukkit.towny.object.Town;
 import com.palmergames.bukkit.towny.object.metadata.BooleanDataField;
+import com.palmergames.bukkit.towny.object.metadata.StringDataField;
 import com.palmergames.bukkit.towny.utils.MetaDataUtil;
 
 import me.NoChance.PvPManager.Dependencies.BaseDependency;
@@ -19,7 +23,8 @@ public class TownyHook extends BaseDependency implements ForceToggleDependency, 
 	private final TownyAPI townyAPI;
 	private final boolean pushbackPeaceful;
 	private final BooleanDataField peaceSetting = new BooleanDataField("siegewar_peaceSetting", false);
-	private static BooleanDataField hasSiege = new BooleanDataField("siegewar_hasSiege", false);
+	private final StringDataField siegeAttackerUUID = new StringDataField("siegewar_attackerUUID", "");
+	private final BooleanDataField hasSiege = new BooleanDataField("siegewar_hasSiege", false);
 
 	public TownyHook(final Hook hook) {
 		super(hook);
@@ -30,16 +35,16 @@ public class TownyHook extends BaseDependency implements ForceToggleDependency, 
 	@Override
 	public boolean shouldDisable(final Player player) {
 		final Resident resident = townyAPI.getResident(player);
-		if (resident == null || !resident.hasTown()) {
+		if (resident == null) {
 			return false;
 		}
 		final Town town = resident.getTownOrNull();
-		return town.hasActiveWar() || hasSiege(town);
+		return town != null && town.hasActiveWar();
 	}
 
 	@Override
 	public boolean shouldDisable(final Player attacker, final Player defender, final CancelResult reason) {
-		return shouldDisable(attacker) && shouldDisable(defender);
+		return shouldDisable(attacker) && shouldDisable(defender) || isAtWarWithNation(attacker, defender);
 	}
 
 	@Override
@@ -51,17 +56,60 @@ public class TownyHook extends BaseDependency implements ForceToggleDependency, 
 		return town == null || !isNeutral(town);
 	}
 
+	private boolean isAtWarWithNation(final Player attacker, final Player defender) {
+		final Resident attackerResident = townyAPI.getResident(attacker);
+		final Resident defenderResident = townyAPI.getResident(defender);
+		if (attackerResident == null || defenderResident == null)
+			return false;
+
+		final Town attackerTown = attackerResident.getTownOrNull();
+		final Town defenderTown = defenderResident.getTownOrNull();
+
+		if (attackerTown == null || defenderTown == null)
+			return false;
+
+		if (hasSiege(attackerTown)) {
+			return isFromAttackingNation(attackerTown, defenderResident);
+		}
+
+		if (hasSiege(defenderTown)) {
+			return isFromAttackingNation(defenderTown, attackerResident);
+		}
+		return false;
+	}
+
+	private boolean isFromAttackingNation(final Town siegedTown, final Resident residentToCheck) {
+		final Nation nationToCheck = residentToCheck.getNationOrNull();
+		if (nationToCheck == null) {
+			return false;
+		}
+		final String attackerUUIDString = getAttackerUUID(siegedTown);
+		if (attackerUUIDString == null || attackerUUIDString.isEmpty()) {
+			return false;
+		}
+
+		final UUID attackerUUID = UUID.fromString(attackerUUIDString);
+		final Nation attackingNation = townyAPI.getNation(attackerUUID);
+		return attackingNation != null && attackingNation.equals(nationToCheck);
+	}
+
 	private boolean isNeutral(final Town town) {
 		if (town.hasMeta(peaceSetting.getKey()))
 			return MetaDataUtil.getBoolean(town, peaceSetting);
 		return town.isNeutral();
 	}
 
-	public boolean hasSiege(final Town town) {
+	private boolean hasSiege(final Town town) {
 		if (town.hasMeta(hasSiege.getKey())) {
 			return MetaDataUtil.getBoolean(town, hasSiege);
 		}
 		return false;
+	}
+
+	private String getAttackerUUID(final Town town) {
+		if (town.hasMeta(siegeAttackerUUID.getKey()))
+			return MetaDataUtil.getString(town, siegeAttackerUUID);
+		return null;
 	}
 
 }
